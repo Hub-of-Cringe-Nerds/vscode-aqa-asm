4d182536a822f826aa767be314625cf22bf4c6f9
RLGingerBiscuit <29359076+RLGingerBiscuit@users.noreply.github.com>
2023-01-09 10:14:07 +0000

    Remove symlink scripts

--- make-link.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-powershell -File "%~dp0/make-link.ps1"
--- make-link.ps1
+++
@@ -1,8 +0,0 @@
-$VSCODE_EXT_FOL = "$HOME/.vscode/extensions"
-$AQA_EXT_FOL = "$VSCODE_EXT_FOL/rlgingerbiscuit-vscode-aqa-asm"
-
-New-Item -ItemType Directory -Force -Path "$VSCODE_EXT_FOL" | Out-Null
-
-Remove-Item -Force -Recurse "$AQA_EXT_FOL" 2>&1 | Out-Null
-
-New-Item -ItemType Junction -Path "$AQA_EXT_FOL" -Target "$PSScriptRoot" | Out-Null
